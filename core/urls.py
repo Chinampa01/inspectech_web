2ec29500b3e74875e0b77fccfbe565c49c6b918f
Chinampa <adrielcuellar1@gmail.com>
2025-07-06 18:39:07 -0300

    servios cargados con la info, se uso formato de cards para mostrarlo en 'home', se agrego el form de contacto

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import HomeView 
+from .views import HomeView
+from . import views
 
 urlpatterns = [
-    path('',HomeView.as_view(), name = 'home'),
+    path('', HomeView.as_view(), name='home'),
+    path('servicio/ensayos-no-destructivos/', views.ServicioEnsayosNoDestructivosView.as_view(), name='servicio_ensayos_no_destructivos'),
+    path('servicio/tratamiento-termico-dureza/', views.ServicioTratamientoTermicoDurezaView.as_view(), name='servicio_tratamiento_termico_dureza'),
+    path('servicio/escuela-soldadura/', views.ServicioEscuelaSoldaduraView.as_view(), name='servicio_escuela_soldadura'),
+    path('servicio/elaboracion-calificacion-procedimientos/', views.ServicioElaboracionCalificacionProcedimientosView.as_view(), name='servicio_elaboracion_calificacion_procedimientos'),
+    path('servicio/metalografia/', views.ServicioMetalografiaView.as_view(), name='servicio_metalografia'),
+    path('servicio/verificacion-materiales/', views.ServicioVerificacionMaterialesView.as_view(), name='servicio_verificacion_materiales'),
+    path('servicio/prueba-vacio-soldaduras/', views.ServicioPruebaVacioSoldadurasView.as_view(), name='servicio_prueba_vacio_soldaduras'),
+    path('servicio/capacitacion-certificacion/', views.ServicioCapacitacionCertificacionView.as_view(), name='servicio_capacitacion_certificacion'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,27 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "home.html"
+
+class ServicioEnsayosNoDestructivosView(TemplateView):
+    template_name = "servicios/ensayos_no_destructivos.html"
+
+class ServicioTratamientoTermicoDurezaView(TemplateView):
+    template_name = "servicios/tratamiento_termico_dureza.html"
+
+class ServicioEscuelaSoldaduraView(TemplateView):
+    template_name = "servicios/escuela_soldadura.html"
+
+class ServicioElaboracionCalificacionProcedimientosView(TemplateView):
+    template_name = "servicios/elaboracion_calificacion_procedimientos.html"
+
+class ServicioMetalografiaView(TemplateView):
+    template_name = "servicios/metalografia.html"
+
+class ServicioVerificacionMaterialesView(TemplateView):
+    template_name = "servicios/verificacion_materiales.html"
+
+class ServicioPruebaVacioSoldadurasView(TemplateView):
+    template_name = "servicios/prueba_vacio_soldaduras.html"
+
+class ServicioCapacitacionCertificacionView(TemplateView):
+    template_name = "servicios/capacitacion_certificacion.html"
